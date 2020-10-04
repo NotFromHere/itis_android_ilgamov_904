f4844c768f614819ca484fb495eb4bdf938d718a
Kap1tan1 <ilsafilsaf66@gmail.com>
2020-10-05 01:38:41 +0300

    hw2_fisrt_commit

--- app/src/main/java/com/example/lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.lesson_1
 
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun renameFun(view: View){
+        var nameField: TextView = findViewById(R.id.textView5)
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val input: EditText = EditText(this)
+
+        builder.setView(input)
+            .setTitle("Изменить имя")
+            .setPositiveButton("Ok",
+            DialogInterface.OnClickListener { dialog, whichButton ->
+                val string: String = input.text.toString()
+                nameField.setText(string)
+            })
+
+        builder.show()
     }
 }

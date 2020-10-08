347439162a4d65440bbf30fbbfef14c7cdabe90e
Kap1tan1 <ilsafilsaf66@gmail.com>
2020-10-08 07:52:08 +0300

    hw3_first_commit

---
+++ app/src/main/java/com/example/lesson_1/Activity_1.kt
@@ -0,0 +1,32 @@
+package com.example.lesson_1
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_one.*
+
+class Activity_1: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one)
+
+        view_action.setOnClickListener(View.OnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("http://developer.alexanderklimov.ru"))
+            startActivityForResult(intent,1)
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == 0)
+            Toast.makeText(this, "NOT RESULT", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(this, "OK RESULT, ReqC:" + requestCode.toString(), Toast.LENGTH_SHORT).show()
+
+    }
+}
---
+++ app/src/main/java/com/example/lesson_1/Activity_2.kt
@@ -0,0 +1,17 @@
+package com.example.lesson_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_one.*
+import kotlinx.android.synthetic.main.activity_second.*
+
+class Activity_2: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}

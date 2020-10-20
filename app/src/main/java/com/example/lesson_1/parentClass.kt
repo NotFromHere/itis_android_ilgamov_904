91001cde970a3465cbcf4f3eeea0551cb7e3ce13
Kap1tan1 <ilsafilsaf66@gmail.com>
2020-10-20 23:00:20 +0300

    cleaning_master

--- app/src/main/java/com/example/lesson_1/parentClass.kt
+++
@@ -1,8 +0,0 @@
-open class parentClass constructor(var field1: String = "someString", var field2: Int = 101){
-    init {
-        println("1. field1 = $field1")
-        println("2. field2 = $field2")
-    }
-
-    fun someFun() = 123
-}

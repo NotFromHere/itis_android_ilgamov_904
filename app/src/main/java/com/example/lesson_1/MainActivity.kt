7ffe8b24a007e22c91ac6faf2663398550982d0a
Kap1tan1 <ilsafilsaf66@gmail.com>
2021-02-22 17:03:41 +0300

    recycler, search and listener added

--- app/src/main/java/com/example/lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.lesson_1
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        search_main.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //req
+                val code: String = "number"
+                if(code.equals("404")){
+                    //Snack-bad
+                    return false
+                } else {
+                    //Snack-ok
+                    return true
+                }
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //nothing
+                return true
+            }
+
+        })
+
     }
 }
+
+
+
+

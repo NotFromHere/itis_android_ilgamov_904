ad10beb262656653782ddd7030ac082ee584cf19
Kap1tan1 <ilsafilsaf66@gmail.com>
2021-03-28 20:03:43 +0300

    mvp CUMplete

--- app/src/main/java/com/example/lesson_1/presenter/activity/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/presenter/activity/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.lesson_1.presenter.activity
 
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
 import android.widget.SearchView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.example.lesson_1.R
+import com.example.lesson_1.data.api.ApiFactory
 import com.example.lesson_1.data.impl.LocationRepositoryImpl
 import com.example.lesson_1.data.impl.WeatherRepositoryImpl
-import com.example.lesson_1.data.api.ApiFactory
 import com.example.lesson_1.domain.entity.CityDomain
 import com.example.lesson_1.domain.usecase.FindCitiesAroundUseCase
 import com.example.lesson_1.presenter.AppDatabase
@@ -23,12 +19,9 @@ import com.example.lesson_1.presenter.recycler.RecyclerCityAdapter
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.launch
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 import moxy.presenter.ProvidePresenter
-import retrofit2.HttpException
-import java.io.*
 
 
 class MainActivity : MvpAppCompatActivity(), MainView {
@@ -65,8 +58,13 @@ class MainActivity : MvpAppCompatActivity(), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rv_main.adapter = rvCityAdapter
+        init()
         mainPresenter.onStart()
+
+    }
+
+    private fun init() {
+        rv_main.adapter = rvCityAdapter
         initListeners()
     }
 
@@ -102,6 +100,14 @@ class MainActivity : MvpAppCompatActivity(), MainView {
             Snackbar.LENGTH_SHORT
         )
             .show()
+    }
+
+    override fun progressOn() {
+        progress.visibility = View.VISIBLE
+    }
+
+    override fun progressOff() {
+        progress.visibility = View.INVISIBLE
     }
 }
 
--- app/src/main/java/com/example/lesson_1/presenter/activity/WeatherActivity.kt
+++ app/src/main/java/com/example/lesson_1/presenter/activity/WeatherActivity.kt
@@ -2,23 +2,31 @@ package com.example.lesson_1.presenter.activity
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import androidx.room.Room
 import com.example.lesson_1.R
-import com.example.lesson_1.data.impl.WeatherRepositoryImpl
 import com.example.lesson_1.data.api.ApiFactory
+import com.example.lesson_1.data.impl.WeatherRepositoryImpl
 import com.example.lesson_1.domain.usecase.GetWeatherUseCase
 import com.example.lesson_1.presenter.AppDatabase
+import com.example.lesson_1.presenter.moxy.WeatherPresenter
+import com.example.lesson_1.presenter.moxy.WeatherView
 import kotlinx.android.synthetic.main.activity_weather.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import moxy.MvpAppCompatActivity
+import moxy.presenter.InjectPresenter
+import moxy.presenter.ProvidePresenter
 
-class WeatherActivity : AppCompatActivity() {
+class WeatherActivity : MvpAppCompatActivity(), WeatherView {
 
-    companion object{
+    companion object {
         val CITY_ID = "KEY_CID"
     }
+
+    @InjectPresenter
+    lateinit var weatherPresenter: WeatherPresenter
+
+    @ProvidePresenter
+    fun providePresenter(): WeatherPresenter = initPresenter()
 
     private val getWeatherUseCase: GetWeatherUseCase by lazy {
         val db = Room.databaseBuilder(
@@ -35,36 +43,58 @@ class WeatherActivity : AppCompatActivity() {
         }
     }
 
+    private fun initPresenter(): WeatherPresenter = WeatherPresenter(getWeatherUseCase)
+
+    private val cityID: Int by lazy { intent.getIntExtra(CITY_ID, -1) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
-        val cityID = intent.getIntExtra(CITY_ID, -1)
-        setupWeather(cityID)
+        weatherPresenter.setupWeather(cityID)
+    }
+
+
+    override fun setupCityName(cityName: String) {
+        tv_city_name.text = cityName
     }
 
     @SuppressLint("SetTextI18n")
-    private fun setupWeather(cityID: Int) {
-        CoroutineScope(Dispatchers.Main).launch {
-            val weather = getWeatherUseCase.getWeather(cityID)
-            tv_city_name.text = weather.cityName
-            tv_temp.text = tv_temp.text.toString() + weather.temp.toInt() + "°C"
-            tv_temp_fill.text =
-                tv_temp_fill.text.toString() + " " +  weather.tempFeel.toInt() + "°C"
-            tv_cloudy.text = tv_cloudy.text.toString() + weather.cloudy
-            tv_humidity.text = tv_humidity.text.toString() + weather.humidity + "%"
-            val windDeg = weather.wind
-            when {
-                windDeg < 45 -> tv_wind.text = tv_wind.text.toString() + "Север"
-                windDeg < 90 -> tv_wind.text = tv_wind.text.toString() + "Северо-Восток"
-                windDeg < 135 -> tv_wind.text = tv_wind.text.toString() + "Восток"
-                windDeg < 180 -> tv_wind.text = tv_wind.text.toString() + "Юго-Восток"
-                windDeg < 225 -> tv_wind.text = tv_wind.text.toString() + "Юг"
-                windDeg < 270 -> tv_wind.text = tv_wind.text.toString() + "Юго-Запад"
-                windDeg < 315 -> tv_wind.text = tv_wind.text.toString() + "Запад"
-                windDeg < 360 -> tv_wind.text = tv_wind.text.toString() + "Северо-Запад"
-                else -> tv_wind.text = tv_wind.text.toString() + "Север"
-            }
-            tv_pressure.text = tv_pressure.text.toString() + weather.pressure + " мм рт. ст."
-        }
+    override fun setupTemp(temp: String) {
+        tv_temp.text = tv_temp.text.toString() + temp
     }
+
+    @SuppressLint("SetTextI18n")
+    override fun setupTempFeel(tempFeel: String) {
+        tv_temp_fill.text =
+            tv_temp_fill.text.toString() + " " + tempFeel
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun setupCloudy(cloudy: String) {
+        tv_cloudy.text = tv_cloudy.text.toString() + cloudy
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun setupHumidity(humidity: String) {
+        tv_humidity.text = tv_humidity.text.toString() + humidity
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun setupWind(wind: String) {
+        tv_wind.text = tv_wind.text.toString() + wind
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun setupPressure(pressure: String) {
+        tv_pressure.text = tv_pressure.text.toString() + pressure
+    }
+
+    override fun progressOn() {
+        progress.visibility = View.VISIBLE
+    }
+
+    override fun progressOff() {
+        progress.visibility = View.INVISIBLE
+    }
+
 }
